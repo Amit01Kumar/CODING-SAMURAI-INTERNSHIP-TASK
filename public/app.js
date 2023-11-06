68a7f84f052abef29e27be3e151870d7b4aed015
Amit Kumar <94733842+Amit01Kumar@users.noreply.github.com>
2023-11-06 08:07:25 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -41,32 +41,7 @@ function deleteTask(taskElement) {
   taskElement.remove();
 }
 
-/*function deleteCompletedTasks() {
-  const completedTasks = document.querySelectorAll('.completed');
-  completedTasks.forEach(task => {
-    deletedTasks.push(task.outerHTML);
-    task.remove();
-  });
-}*/
 
-/*function undoDelete() {
-  if (deletedTasks.length > 0) {
-    const lastDeletedTask = deletedTasks.pop();
-    const tempContainer = document.createElement('div');
-    tempContainer.innerHTML = lastDeletedTask;
-    const restoredTask = tempContainer.firstChild;
-
-    // Remove the "completed" class to unstrike the task
-    restoredTask.classList.remove('completed');
-
-    const restoredDeleteButton = restoredTask.querySelector('button');
-    restoredDeleteButton.addEventListener('click', () => {
-      deleteTask(restoredTask);
-    });
-
-    taskList.insertAdjacentElement('beforeend', restoredTask);
-  }
-}*/
 function deleteCompletedTasks() {
   const completedTasks = document.querySelectorAll('.completed');
   completedTasks.forEach(task => {
